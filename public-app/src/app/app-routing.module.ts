d1751794f7315bfa63142337aec351ab2e0fb7e8
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-07-17 03:05:37 +0200

    login and register form design started

--- public-app/src/app/app-routing.module.ts
+++ public-app/src/app/app-routing.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
-  { path: 'login',      component: LoginComponent    },
-  { path: 'register',   component: RegisterComponent },
-  //{ path: '**',         redirectTo: '/login' }
+  { path: 'landing', component: AuthComponent },
 ];
 
 @NgModule({
--- public-app/src/app/app.module.ts
+++ public-app/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MainModule } from './main/main.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    BrowserAnimationsModule,
     MainModule,
     FontAwesomeModule
   ],
