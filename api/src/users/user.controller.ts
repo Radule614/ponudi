fe249a976bdc6e2443d01b5a818cd38068f9ed69
Srdjan Stjepanovic <businessrdjan@gmail.com>
2022-07-19 19:04:12 +0200

    Added authorization guard

--- api/src/users/user.controller.ts
+++ api/src/users/user.controller.ts
@@ -3,6 +3,9 @@ import { UserService } from "./user.service";
 import { ErrorInterceptor } from "src/interceptors/error.interceptor";
 import { MongoErrorFilter } from "src/errorFilters/mongo-error.filter";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { Roles } from "src/auth/role.decorator";
+import { UserRole } from "./enums/user-role.enum";
+import { RolesGuard } from "src/auth/guards/role.guard";
 
 @Controller('users')
 @UseInterceptors(ErrorInterceptor)
@@ -14,7 +17,8 @@ export class UserController {
     ) { }
 
     @Get('/')
-    @UseGuards(JwtAuthGuard)
+    @Roles(UserRole.ADMIN)
+    @UseGuards(JwtAuthGuard, RolesGuard)
     async getAllUsers() {
         return await this.userService.findAll()
     }
