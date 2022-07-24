8f1833fa604d1a56a840e3c4d52d40477dfa0bb3
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-07-24 18:57:18 +0200

    article list basic design

--- public-app/src/app/main/pages/category/category.component.ts
+++ public-app/src/app/main/pages/category/category.component.ts
@@ -6,17 +6,42 @@ import { Article } from "src/app/model/article.model";
 import { AppState } from "src/app/store";
 import * as ArticleSelectors from '../../../store/article/article.selectors';
 import * as FromArticle from '../../../store/article/article.actions';
+import { animate, group, query, state, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.scss']
+  styleUrls: ['./category.component.scss'],
+  animations: [
+    trigger('expandBlock', [
+      transition(':enter', [
+        style({ height: 0}),
+        animate('250ms ease-out', style({ height: '*' })),
+      ]),
+      transition(':leave', [
+        style({ height: '*'}),
+        group([
+          animate('250ms ease-out', style({ height: 0 })),
+          query('.inner', [
+            style({ opacity: 1}),
+            animate(200, style({ opacity: 0 })),
+          ])
+        ])
+      ])
+    ]),
+    trigger('expandButton', [
+      state('true',   style({ transform: 'rotateZ(-90deg)' })),
+      state('false',  style({ transform: 'rotateZ(0deg)' })),
+      transition('true <=> false', animate('250ms ease-out'))
+    ])
+  ]
 })
 export class CategoryComponent implements OnInit, OnDestroy {
   categoryId: string = "";
   subs: Subscription[] = [];
 
   articles: Article[] = [];
+  filtersExpanded: boolean = false;
 
   constructor(private route: ActivatedRoute, private store: Store<AppState>){}
 
