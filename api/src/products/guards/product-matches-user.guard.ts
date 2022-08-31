eb384831492d4f4e0e6c141c9f6c28d5350f91b6
srdjan1210 <businessrdjan@gmail.com>
2022-08-31 17:26:42 +0200

    Added guard for email verification

--- api/src/products/guards/product-matches-user.guard.ts
+++ api/src/products/guards/product-matches-user.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable } from "@nestjs/common";
 import { Observable } from "rxjs";
 
 
@@ -8,7 +8,7 @@ export class ProductMatchesUser implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         let req: any = context.switchToHttp().getRequest()
         let { user, product } = req
-        if (!user || !product || user.id != product.owner) return false
+        if (!user || !product || user.id != product.owner) throw new ForbiddenException('Product should match user!')
         return true
     }
 }
