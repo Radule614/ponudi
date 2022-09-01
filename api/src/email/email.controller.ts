e98464ae1337649f5754c46786a1587da0638265
srdjan1210 <businessrdjan@gmail.com>
2022-09-01 22:03:59 +0200

    Updated request type for email

--- api/src/email/email.controller.ts
+++ api/src/email/email.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, Inject, Param, Query } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, Inject, Param, Post, Query } from '@nestjs/common';
 import { NoResourceException } from 'src/exceptions/no-resource.exception';
 import { UserService } from 'src/users/user.service';
 import { v4 } from "uuid"
@@ -10,13 +10,13 @@ export class EmailController {
     constructor(private readonly emailService: EmailService) { }
 
 
-    @Get('/verify')
-    async verifyEmail(@Query('token') token: string) {
+    @Post('/verify')
+    async verifyEmail(@Body('token') token: string) {
         await this.emailService.verifyEmail(token)
     }
 
-    @Get('/resend')
-    async resendVerification(@Query('email') email: string) {
+    @Post('/resend')
+    async resendVerification(@Body('email') email: string) {
         await this.emailService.sendVerificationEmail(email)
     }
 }
